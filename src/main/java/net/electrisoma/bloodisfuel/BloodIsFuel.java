c979a204a42e4b94381aa4767f5c86d5b05be99f
Electrisoma <electrisoma@gmail.com>
2025-01-01 06:59:56 -0600

    cleaned some stuff up

--- src/main/java/net/electrisoma/bloodisfuel/BloodIsFuel.java
+++ src/main/java/net/electrisoma/bloodisfuel/BloodIsFuel.java
@@ -5,10 +5,7 @@ import net.electrisoma.bloodisfuel.register.*;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
-import static com.simibubi.create.Create.REGISTRATE;
 
 
 @Mod(BloodIsFuel.MOD_ID)
@@ -18,7 +15,6 @@ public class BloodIsFuel {
     public static final CreateRegistrate REGISTRATE = CreateRegistrate.create("bloodisfuel");
     public BloodIsFuel() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
 
         Fluids.register();
         CreativeTab.register(modEventBus);
